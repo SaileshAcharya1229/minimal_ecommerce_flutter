9de7f06b27e197cc8cd005d5fb49d4ab5082bee6
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-11-07 20:33:20 +0545

    Adding logo

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -7,6 +7,32 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.shopping_bag,
+              size: 72,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 25),
+
+            //title
+            const Text(
+              "Minimal Shop",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+
+            //subtitle
+            const Text("Premium Quality Products")
+
+            //button
+          ],
+        ),
+      ),
     );
   }
 }
